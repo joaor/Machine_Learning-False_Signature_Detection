9ef1bc75f4f95855ae0fab9bb121daf2f980ff58
João Rodrigues <joao.marques.rodrigues@gmail.com>
2010-11-04 18:34:10 +0000

    Funcao main limpa

--- load_files.m
+++ load_files.m
@@ -7,23 +7,16 @@ for signature = 1:300
         genuine = [path,num2str(signature,'%.3d'),'/c-',fname,'.bmp'];
         forged = [path,num2str(signature,'%.3d'),'/cf-',fname,'.bmp'];
         
-        if eq(exist(genuine),2)
-            I=imread(genuine);
-            figure(1);
-            [C,H] = imcontour(I)
-            contornos = ones(size(I,1), size(I,2));
-            for i = 2:2:size(C,2)
-                contornos(int16(C(2,i)),int16(C(1,i))) = 0;
-            end
-            figure(2);
-            imshow(contornos);
+        if eq(exist(genuine,'file'),2)
+            get_features(genuine);
             pause
         else
             fprintf('Fichero de assinatura genuina %g nao encontrado\n',sample)
         end
         
-        if eq(exist(forged),2)
-            I=imread(forged);
+        if eq(exist(forged,'file'),2)
+            get_features(genuine);
+            pause
         else
             fprintf('Fichero e assinatura falsa %g nao encontrado\n',sample)
         end  
