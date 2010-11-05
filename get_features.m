7eb33c33ecef7bdc3b238b53f6e9ed1657d0fcca
João Rodrigues <joao.marques.rodrigues@gmail.com>
2010-11-05 16:20:44 +0000

    Novas features introduzidas

--- get_features.m
+++ get_features.m
@@ -1,8 +1,9 @@
-function get_features(img)
+function feat = get_features(img)
     im = imread(img);
     image = invert_image(im2bw(im));
     crop_image = surface_image(image);
-    centroid_feature = centroid_feat(crop_image)
-    trisurface_feat = trisurface_feature(crop_image)
+    centroid_features = centroid_feat(crop_image);
+    trisurface_feat = trisurface_feature(crop_image);
+    length_feature = length_feat(crop_image);
+    sixfold_surface_feature = six_fold_feat(crop_image)
 end
-
